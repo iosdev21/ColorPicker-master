2c44a74eb90c262acea3ce92d471715a47e6ed1b
IOS Dev <ios.dev.21@outlook.com>
2017-04-03 21:22:27 -0400

    Timer is added!!!

--- TestSwiftProject/ViewController.swift
+++ TestSwiftProject/ViewController.swift
@@ -11,10 +11,19 @@ import UIKit
 class ViewController: UIViewController,colorDelegate {
     @IBOutlet var colorPicker: ColorPicker!
     @IBOutlet var colorView: UIView!
+    @IBOutlet weak var canvasView: UIView!
 
+    @IBOutlet weak var timeLabel: UILabel!
+    
+    var seconds = 180
+    var timer = Timer()
+    var isTimerRunning = false
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         colorPicker.delegate = self;
+        
+        runTimer();
         
         // Do any additional setup after loading the view, typically from a nib.
     }
@@ -26,6 +35,32 @@ class ViewController: UIViewController,colorDelegate {
         // Dispose of any resources that can be recreated.
     }
 
+    func runTimer() {
+        timer = Timer.scheduledTimer(timeInterval: 1, target: self,   selector: (#selector(ViewController.updateTimer)), userInfo: nil, repeats: true)
+    }
 
+    func updateTimer() {
+        seconds -= 1     //This will decrement(count down)the seconds.
+        timeLabel.text = timeString(time: TimeInterval(seconds))
+        
+        if (seconds == 0) {
+            seconds = 180
+        }
+    }
+
+    func resetTimer() {
+        timer.invalidate()
+        seconds = 180
+        timeLabel.text = timeString(time: TimeInterval(seconds))
+    }
+    
+    func timeString(time:TimeInterval) -> String {
+        let minutes = Int(time) / 60 % 60
+        let seconds = Int(time) % 60
+        return String(format: "%02i : %02i", minutes, seconds)
+    }
+    
+    
+    
 }
 
